77cea6c8ff63eeffa6ae7c1a816a0dd92fb06b31
Frederik Loeffert <frederik@zitrusmedia.de>
2013-09-19 20:26:48 +0200

    MimeType object added

---
+++ lib/Foomo/ContentServer/MimeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Foomo\ContentServer;
+
+class MimeType
+{
+	//---------------------------------------------------------------------------------------------
+	// ~ Constants
+	//---------------------------------------------------------------------------------------------
+
+	const MIME_RADACT_RAD_DOC		= 'application/x-radactRaddoc5';
+	const MIME_RADACT_TEXT			= 'application/x-radactText';
+	const MIME_RADACT_LINKER		= 'application/x-radactLinker';
+	const MIME_RADACT_APP			= 'application/x-radactApp';
+	const MIME_RADACT_FOLDER		= 'application/x-radactFolder';
+	const MIME_RADACT_LINK			= 'application/x-radactLink';
+
+	const MIME_OCTET_STREAM			= 'application/octet-stream';
+	const MIME_ZIP					= 'application/x-zip';
+	const MIME_PDF					= 'application/pdf';
+	const MIME_FLASH				= 'application/x-shockwave-flash';
+
+	const MIME_IMAGE_GIF			= 'image/gif';
+	const MIME_IMAGE_JPEG			= 'image/jpeg';
+	const MIME_IMAGE_PNG			= 'image/png';
+
+	const MIME_TEXT_HTML			= 'text/html';
+	const MIME_TEXT_PLAIN           = 'text/plain';
+	const MIME_TEXT_XML             = 'text/xml';
+	const MIME_FLASH_VIDEO			= 'video/x-flv';
+
+}
