105654391dbd2a3f24385c9b053832e0ce03135c
Frederik Loeffert <frederik@zitrusmedia.de>
2013-12-03 11:36:15 +0100

    version fixed to 0.1.2

--- lib/Foomo/ContentServer/Module.php
+++ lib/Foomo/ContentServer/Module.php
@@ -28,11 +28,8 @@ class Module extends \Foomo\Modules\ModuleBase
 	//---------------------------------------------------------------------------------------------
 	// ~ Constants
 	//---------------------------------------------------------------------------------------------
-	const VERSION = '0.1.0';
-	/**
-	 * the name of this module
-	 *
-	 */
+
+	const VERSION = '0.1.2';
 	const NAME = 'Foomo.ContentServer';
 
 	//---------------------------------------------------------------------------------------------
@@ -65,14 +62,6 @@ class Module extends \Foomo\Modules\ModuleBase
 	{
 		return array(
 			\Foomo\Modules\Resource\Module::getResource('Foomo.SimpleData', '0.3.*')
-			// get a run mode independent folder var/<runMode>/test
-			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'test'),
-			// and a file in it
-			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_File, 'test' . DIRECTORY_SEPARATOR . 'someFile'),
-			// request a cache resource
-			// \Foomo\Modules\Resource\Fs::getCacheResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'navigationLeaves'),
-			// a database configuration
-			// \Foomo\Modules\Resource\Config::getResource('yourModule', 'db')
 		);
 	}
 	public static function hookPostConfigUpdate($oldConfig, $newConfig, $module, $domain)
